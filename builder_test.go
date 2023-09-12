f8c02cb611b55c2633edf9a071a54ae8dc089bcf
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2023-09-13 01:09:04 +0900

    added test for Advance

--- builder_test.go
+++ builder_test.go
@@ -2,6 +2,7 @@ package synchro_test
 
 import (
 	"fmt"
+	"testing"
 	"time"
 
 	"github.com/Code-Hex/synchro"
@@ -69,4 +70,51 @@ func ExampleTime_Advance() {
 	// 2010-12-12 00:01:00 +0000 UTC
 	// 2010-12-12 00:01:01 +0000 UTC
 	// 2010-12-12 00:01:01.123456789 +0000 UTC
+}
+
+func TestAdvance(t *testing.T) {
+	utc := synchro.New[tz.UTC](2009, time.November, 10, 23, 0, 0, 0)
+
+	t.Run("month twice", func(t *testing.T) {
+		got := utc.Advance().Month(1).Month(2).Do() // +3 months
+		want := synchro.New[tz.UTC](2010, time.February, 10, 23, 0, 0, 0)
+		if want != got {
+			t.Fatalf("- %s\n+ %s", want, got)
+		}
+	})
+	t.Run("day twice", func(t *testing.T) {
+		got := utc.Advance().Day(1).Day(2).Do() // +3 days
+		want := synchro.New[tz.UTC](2009, time.November, 13, 23, 0, 0, 0)
+		if want != got {
+			t.Fatalf("- %s\n+ %s", want, got)
+		}
+	})
+	t.Run("hour twice", func(t *testing.T) {
+		got := utc.Advance().Hour(1).Hour(2).Do() // +3 hours
+		want := synchro.New[tz.UTC](2009, time.November, 11, 2, 0, 0, 0)
+		if want != got {
+			t.Fatalf("- %s\n+ %s", want, got)
+		}
+	})
+	t.Run("minute twice", func(t *testing.T) {
+		got := utc.Advance().Minute(5).Minute(60).Do() // +65 minutes
+		want := synchro.New[tz.UTC](2009, time.November, 11, 0, 5, 0, 0)
+		if want != got {
+			t.Fatalf("- %s\n+ %s", want, got)
+		}
+	})
+	t.Run("second twice", func(t *testing.T) {
+		got := utc.Advance().Second(5).Second(60).Do() // +65 seconds
+		want := synchro.New[tz.UTC](2009, time.November, 10, 23, 1, 5, 0)
+		if want != got {
+			t.Fatalf("- %s\n+ %s", want, got)
+		}
+	})
+	t.Run("nanosec twice", func(t *testing.T) {
+		got := utc.Advance().Nanosecond(5).Nanosecond(60).Do() // +65 nanosec
+		want := synchro.New[tz.UTC](2009, time.November, 10, 23, 0, 0, 65)
+		if want != got {
+			t.Fatalf("- %s\n+ %s", want, got)
+		}
+	})
 }
